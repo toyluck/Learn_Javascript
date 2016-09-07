91d3a84a7bda0e45987677891d9f228bb7810f2b
toyluck <toylucklebo@outlook.com>
2016-09-07 09:33:11 +0800

    add .idea

--- arrays.js
+++ arrays.js
@@ -3,3 +3,18 @@ console.log(myArr.length);
 
 myArr[10000]=10000;
 console.log(myArr.length);
+
+
+
+(
+     function foo(){
+        var h_time;
+        return function(time){
+            h_time= "time = "+ time;
+            return h_time;
+        }
+     }
+
+    var foo=foo();
+    foo("time");
+)
